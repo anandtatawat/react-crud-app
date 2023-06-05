fbab0546d4abc2fe59b203f1010061b0b46ece8a
Anand <anandtatawat131@gmail.com>
2023-06-05 20:57:44 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import EmpListing from "./EmpListing";
+import EmpCreate from "./EmpCreate";
+import EmpDetail from "./EmpDetail";
+import EmpEdit from "./EmpEdit";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>REACT JS CRUD OPERATIONS</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<EmpListing/>}></Route>
+          <Route path="/employee/create" element={<EmpCreate/>}></Route>
+          <Route path="/employee/detail/:empid" element={<EmpDetail/>}></Route>
+          <Route path="/employee/edit/:empid" element={<EmpEdit/>}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+ /* <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<EmpListing />}></Route>
+    </Routes>
+  </BrowserRouter>;*/
+}
+
+export default App;
